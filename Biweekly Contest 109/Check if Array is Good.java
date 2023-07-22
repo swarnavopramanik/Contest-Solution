ff0f4f6c32ef91703df173ef63cd956dd4ca5da9
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-07-22 21:47:30 +0530

    Update Check if Array is Good.java

--- Biweekly Contest 109/Check if Array is Good.java
+++ Biweekly Contest 109/Check if Array is Good.java
@@ -1,1 +1,13 @@
-(https://leetcode.com/contest/biweekly-contest-109/problems/check-if-array-is-good/)
+https://leetcode.com/contest/biweekly-contest-109/problems/check-if-array-is-good/
+
+class Solution {
+    public boolean isGood(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        nums[n-1]++;
+        for(int i = 0;i < n;i++){
+            if(nums[i] != i+1)return false;
+        }
+        return true;
+    }
+}
