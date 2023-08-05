d69ae366c4314a6b7608607e710281a36355ae2a
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-05 21:49:09 +0530

    Create Account Balance After Rounded Purchase.java

---
+++ Biweekly Contest 110/Account Balance After Rounded Purchase.java
@@ -0,0 +1,47 @@
+2806. Account Balance After Rounded Purchase
+
+  class Solution {
+    
+    public int accountBalanceAfterPurchase(int purchaseAmount) {
+        
+        int balt=100;
+        if(purchaseAmount==0){
+            return 100;
+        }
+        
+        if(purchaseAmount%10==0)
+        {
+            return balt-purchaseAmount;
+        }
+        
+        int rem=purchaseAmount%10;
+        
+        int div=purchaseAmount/10;
+        
+      
+        
+        if(rem>=5){
+            
+            div++;
+        }
+        
+        int amtr=div*10;
+        
+        balt-=amtr;
+        
+        return balt;
+        
+    }
+}
+
+//
+
+class Solution {
+    public int accountBalanceAfterPurchase(int p) {
+        int a = p / 10 * 10;
+        int b = a + 10;
+        if (p - a < b - p) return 100 - a;
+        else return 100 - b;
+    }
+}
+
