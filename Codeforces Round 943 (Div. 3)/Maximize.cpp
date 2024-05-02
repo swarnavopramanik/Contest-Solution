0c83daf8b38082da1db3ce4f716c9b4a30f35c26
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2024-05-02 22:35:48 +0530

    Create Maximize.cpp

---
+++ Codeforces Round 943 (Div. 3)/Maximize.cpp
@@ -0,0 +1,18 @@
+https://codeforces.com/contest/1968/problem/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main() {
+       int t;
+    cin >> t;
+    while (t--) {
+        int x;
+        cin >> x;
+        cout << x - 1 << endl;  
+    }
+    return 0;
+}
